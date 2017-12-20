3b07cad0618cb03915a583da6889db7052c6d84c
natborks <natborksdeutsch@yahoo.com>
2017-12-20 00:11:26 +0000

    installed bootstrap gem

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,5 @@ module Organizer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-config.assets.initialize_on_precompile = false
+
 end
